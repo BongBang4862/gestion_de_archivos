5af2ca47a7d64cd7545f624c666d51a32af65494
BongBang <luisc.olavarria@MBP-de-BongBang.home>
2024-10-13 13:44:37 +0200

    configuracion de alertas con sweetalert2

--- Assets/js/custom.js
+++ Assets/js/custom.js
@@ -1,1 +1,11 @@
 // Here goes your custom javascript
+alertaPersonalizada('success','Primera alerta creada')
+function alertaPersonalizada(type,mensaje) { 
+    Swal.fire({
+      position: "top-end",
+      icon: type,
+      title:  mensaje,
+      showConfirmButton: false,
+      timer: 1500
+    });
+}
